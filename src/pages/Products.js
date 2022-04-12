46bb687c65df3193e5cb811f041cbb3427b7f714
Albertabrahams <halilibrahim.soyman@gmail.com>
2022-04-13 02:18:39 +0300

    project done

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,39 +1,45 @@
 
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styles from "./Products.module.css";
 import { AiFillPlusCircle } from 'react-icons/ai';
-import { async } from "@firebase/util";
 import axios from "axios";
 
 
 const Products = ({products, baseUrl, fetchTasks}) => {
+  const navigate = useNavigate();
 
   const HandleDelete = async (e)=>{ 
-    await axios.delete(`${baseUrl}/${e.target.id}`)
+    await axios.delete(`${baseUrl}/${e.target.parentElement.parentElement.id}`)
+    
+    fetchTasks();
   }
+
 
   return (
     <div>
       
       <div className={styles.products}>
         {products.map((each) => (
-          <Link
-            to={`details/${each.id}`}
-            key={each.id}
+
+          <span
+            id={each.id}
             className={styles.product}
           >
-              <div className="card mt-5">
-  <img className="card-img-top" src={each.avatar} style={{width: "15rem", height:"15rem"}} alt="Card image cap"/>
-  <div className="card-body">
-    <h5 className="card-title font-weight-bold">{each.name}</h5>
-    <p className="card-text text-center font-weight-bold">${each.price}</p>
-    <a href="#" className="btn btn-primary" onClick={HandleDelete}>Delete</a>
-  </div>
-</div>
-            {/* <img className={styles.img} style={{ width: "100px" }} src={each.avatar} alt="" />
-            <h6 className={styles.name}>{each.name}</h6>
-            <p className={styles.price}>${each.price}</p> */}
-          </Link>
+              <div className="card mt-5" onClick={()=>navigate(`details/${each.id}`)} >
+            <img 
+            className="card-img-top" src={each.avatar} 
+            style={{width: "15rem", height:"15rem"}} 
+            alt="Card image cap"
+            
+            />
+            <div className="card-body">
+              <h5 className="card-title font-weight-bold">{each.name}</h5>
+              <p className="card-text text-center font-weight-bold">${each.price}</p>
+            </div>
+          </div>
+          <div><a href="#" className="btn btn-primary" onClick={HandleDelete}>Delete</a></div>
+          </span>
+  
         ))}
       </div>
       <Link
